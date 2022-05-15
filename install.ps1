e991b06c564b917b6cba08e10ebe79a849ffcf1b
jojoko0815 <105592498+jojoko0815@users.noreply.github.com>
2022-05-15 15:13:55 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -4,5 +4,6 @@ start https://www.google.com/chrome/
 start https://cdn.cloudflare.steamstatic.com/client/installer/SteamSetup.exe
 start https://www.ascomp.de/de/download/sEraser.exe
 start https://www.teamviewer.com/de/automatischer-download-von-teamviewer/
+start https://discord.com/api/downloads/distributions/app/installers/latest?channel=stable&platform=win&arch=x86
 echo Fertig
 pause>0

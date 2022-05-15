1c2cce33056d2c6668ab56d4d4be9d0ec7980127
jojoko0815 <105592498+jojoko0815@users.noreply.github.com>
2022-05-15 15:08:39 +0200

    Create install.ps1

---
+++ install.ps1
@@ -0,0 +1,8 @@
+title Windows 10 Setup
+start https://launcher-public-service-prod06.ol.epicgames.com/launcher/api/installer/download/EpicGamesLauncherInstaller.msi
+start https://www.google.com/chrome/
+start https://cdn.cloudflare.steamstatic.com/client/installer/SteamSetup.exe
+start https://www.ascomp.de/de/download/sEraser.exe
+start https://www.teamviewer.com/de/automatischer-download-von-teamviewer/
+echo Fertig
+pause>0

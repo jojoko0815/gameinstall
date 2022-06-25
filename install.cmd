1437d38d069998d21efcf80d85621030db38273b
jojoko0815 <105592498+jojoko0815@users.noreply.github.com>
2022-06-25 17:59:37 +0200

    Update install.cmd

--- install.cmd
+++ install.cmd
@@ -1,11 +1,18 @@
 title Windows 10 Setup
 cmd
 start https://launcher-public-service-prod06.ol.epicgames.com/launcher/api/installer/download/EpicGamesLauncherInstaller.msi
+echo Download von Epic Games Store startet
 start https://www.google.com/chrome/
+echo Download von Chrome startet nicht. Drücke manuell auf Download
 start https://cdn.cloudflare.steamstatic.com/client/installer/SteamSetup.exe
+echo Download von Steam startet
 start https://www.ascomp.de/de/download/sEraser.exe
+echo Download von Secure Eraser startet
 start https://www.teamviewer.com/de/automatischer-download-von-teamviewer/
+echo Download von Team Viewer startet
 start https://discord.com/api/downloads/distributions/app/installers/latest?channel=stable&platform=win&arch=x86
+echo Download von Discord startet
 start https://order.anydesk.com/trial?prod=performance&lang=de
+echo Download von Anydesk startet
 echo Fertig
 pause>0
